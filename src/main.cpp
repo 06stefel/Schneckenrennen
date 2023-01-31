0c8939abc93c5d0f47dbfe694ee0bdc8421ee96b
06stefel <120563914+06stefel@users.noreply.github.com>
2023-01-31 08:42:13 +0100

    main merge1

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <Windows.h>
+#include <fstream>
+#include "simplefunctions.h"
+// simplesnippets im suchfeld eingeben für die snippet datei
+
+using namespace std;
+
+//=============================================================================
+//                           MAIN
+//=============================================================================
+int main()
+{
+    srand(time(NULL));
+    SetConsoleOutputCP(CP_UTF8);
+    cout << "Hello World";
+//write a code that adds two numbers  str shift i   nor mochts  
+
+//    string pfad = "example.txt";     //Beistpeipfad brauchts für dateiauslesen für übergabe zu zeilelesen Funktion
+    // fstream datei;
+    // string pfad = "..\\src\\daten.csv";
+    // datei.open(pfad);
+
+    // datei.seekp(0, ios::end);     // Öffnet die Datei vom Ende her..
+    // int filesize = datei.tellg(); // gibt die absolute Position=Anzahl an Bytes zurück..
+    // datei.seekp(0, ios::beg); // Öffnet die Datei vom Ende her..
+
+
+    
+    return 0;
+}
+
+//=============================================================================
+//                           END OF FILE
+//=============================================================================
